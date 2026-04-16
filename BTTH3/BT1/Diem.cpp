9ee61e366cf462889f6bbc1e841100956143790b
thienpq2007-afk <thienpq2007@gmail.com>
2026-04-16 22:04:39 +0700

    Update Diem.cpp

--- BTTH3/BT1/Diem.cpp
+++ BTTH3/BT1/Diem.cpp
@@ -1,1 +1,52 @@
+#include<iostream>
+#include<cmath>
+#include"Diem.h"
 
+cDiem::cDiem()
+{
+	x = y = 0;
+}
+
+cDiem::cDiem(double x, double y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+void cDiem::Nhap()
+{
+	cout << "Nhap x: ";
+	cin >> x;
+	cout << "Nhap y: ";
+	cin >> y;
+}
+
+void cDiem::Xuat() const
+{
+	cout << "(" << x << ", " << y << ")" << "; ";
+}
+
+double cDiem::getX() const
+{
+	return x;
+}
+
+double cDiem::getY() const
+{
+	return y;
+}
+
+void cDiem::setX(double x)
+{
+	this->x = x;
+}
+
+void cDiem::setY(double y)
+{
+	this->y = y;
+}
+
+double cDiem::KhoangCach(const cDiem& d) const
+{
+	return sqrt(pow(x - d.x, 2) + pow(y - d.y, 2));
+}
