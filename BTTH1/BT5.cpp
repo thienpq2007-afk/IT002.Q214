a7f987167f60d09553fc7f098cb2f8c197ff198a
thienpq2007-afk <thienpq2007@gmail.com>
2026-04-16 20:25:55 +0700

    Update BT5.cpp

--- BTTH1/BT5.cpp
+++ BTTH1/BT5.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+struct ThongTinHocSinh
+{
+	string Hoten;
+	float DiemToan;
+	float DiemVan;
+};
+
+void Nhap(ThongTinHocSinh& x)
+{
+	getline(cin, x.Hoten);
+	cin >> x.DiemToan >> x.DiemVan;
+}
+
+void Xuat(ThongTinHocSinh x)
+{
+	float DTB = (x.DiemToan + x.DiemVan) / 2;
+
+	cout << x.Hoten << "\t";
+	cout << x.DiemToan << "\t";
+	cout << x.DiemVan << "\t";
+	cout << DTB;
+}
+
+int main()
+{
+	ThongTinHocSinh x;
+
+	Nhap(x);
+	Xuat(x);
+
+	return 0;
+}
