b6bb2d064801d9d09017acac7c145890441f1398
thienpq2007-afk <thienpq2007@gmail.com>
2026-04-16 22:04:13 +0700

    Update TamGiac.h

--- BTTH3/BT1/TamGiac.h
+++ BTTH3/BT1/TamGiac.h
@@ -1,1 +1,22 @@
+#pragma once
+#include<iostream>
+#include"Diem.h"
+using namespace std;
+
+class cTamGiac
+{
+private:
+	cDiem A, B, C;
+
+public:
+	void Nhap();
+	void Xuat();
+	double ChuVi();
+	double DienTich();
+	bool LaTamGiac();
+	void KiemTraLoai();
+	void TinhTien(double dx, double dy);
+	void PhongTo(double k);
+	void ThuNho(double k);
+};
 
