c018119aca3a68a72e11d1ccbdaf479f3560bac0
thienpq2007-afk <thienpq2007@gmail.com>
2026-04-16 22:03:44 +0700

    Update Diem.h

--- BTTH3/BT1/Diem.h
+++ BTTH3/BT1/Diem.h
@@ -1,1 +1,20 @@
+#pragma once
+#include<iostream>
+using namespace std;
 
+class cDiem
+{
+private:
+	double x, y;
+
+public:
+	cDiem();
+	cDiem(double x, double y);
+	void Nhap();
+	void Xuat() const;
+	double getX() const;
+	double getY() const;
+	void setX(double x);
+	void setY(double y);
+	double KhoangCach(const cDiem& d) const;
+};
