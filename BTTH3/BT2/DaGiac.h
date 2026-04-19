07969d6a271d584022727643d8486c607dbbcec3
thienpq2007-afk <thienpq2007@gmail.com>
2026-04-19 09:29:11 +0700

    Update DaGiac.h

--- BTTH3/BT2/DaGiac.h
+++ BTTH3/BT2/DaGiac.h
@@ -1,1 +1,23 @@
+#pragma once
+#include "Diem.h"
 
+class cDaGiac
+{
+private:
+    cDiem* A;
+    int n;
+
+public:
+    cDaGiac();
+    ~cDaGiac();
+    cDaGiac(const cDaGiac& other);
+    cDaGiac& operator=(const cDaGiac& other);
+    void Nhap();
+    void Xuat();
+    double ChuVi();
+    double DienTich();
+    void TinhTien(double dx, double dy);
+    void PhongTo(double k);
+    void ThuNho(double k);
+    void Quay(double goc);
+};
