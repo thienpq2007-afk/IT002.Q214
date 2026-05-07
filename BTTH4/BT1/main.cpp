6996c52fe7aa42f4276afe6f0286e43bca2a5580
thienpq2007-afk <thienpq2007@gmail.com>
2026-05-07 09:28:27 +0700

    Update main.cpp

--- BTTH4/BT1/main.cpp
+++ BTTH4/BT1/main.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+#include<iomanip>
+#include"cSoPhuc.h"
+using namespace std;
 
+int main()
+{
+	cSoPhuc A, B;
+	cout << "Nhap so phuc A: " << endl;
+	cin >> A;
+	cout << "Nhap so phuc B: " << endl;
+	cin >> B;
+	cout << "A = " << A << endl;
+	cout << "B = " << B << endl;
+	cout << "Tong 2 so phuc: ";
+	cout << A + B << endl;
+	cout << "Hieu 2 so phuc: ";
+	cout << A - B << endl;
+	cout << "Tich 2 so phuc : ";
+	cout << A * B << endl;
+	cout << "Thuong 2 so phuc: ";
+	cout << setprecision(2) <<  A / B << endl;
+	if (A == B)
+	{
+		cout << "A = B";
+	}
+	else
+		cout << "A != B";
+}
