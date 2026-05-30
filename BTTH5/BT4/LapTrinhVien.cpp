2f4809566f147fa9378178ee4745ae284562710b
thienpq2007-afk <thienpq2007@gmail.com>
2026-05-30 20:10:24 +0700

    Update LapTrinhVien.cpp

--- BTTH5/BT4/LapTrinhVien.cpp
+++ BTTH5/BT4/LapTrinhVien.cpp
@@ -1,5 +1,8 @@
 #include "LapTrinhVien.h"
 
+// Nhập thông tin lập trình viên
+// Input: thông tin nhân viên cơ bản và số giờ làm thêm
+// Output: cập nhật thông tin lập trình viên
 void LapTrinhVien::Nhap()
 {
     NhanVien::Nhap();
@@ -7,6 +10,9 @@ void LapTrinhVien::Nhap()
     cin >> soGioOvertime;
 }
 
+// Xuất thông tin lập trình viên
+// Input: thông tin lập trình viên
+// Output: hiển thị thông tin ra màn hình
 void LapTrinhVien::Xuat()
 {
     cout << "\n===== LAP TRINH VIEN =====" << endl;
@@ -14,6 +20,11 @@ void LapTrinhVien::Xuat()
     cout << "So gio overtime: " << soGioOvertime << endl;
 }
 
+// Tính lương lập trình viên
+// Input: lương cơ bản, số giờ làm thêm
+// Output: tiền lương của lập trình viên
+// Thuật giải:
+// Lương = lương cơ bản + số giờ overtime × 200000
 double LapTrinhVien::TinhLuong()
 {
     return luongCoBan + soGioOvertime * 200000;
