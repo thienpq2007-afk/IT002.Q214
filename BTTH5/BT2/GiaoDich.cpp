f367ea481cd0f357281fc9017a4c24ddda0f9163
thienpq2007-afk <thienpq2007@gmail.com>
2026-05-30 20:01:48 +0700

    Update GiaoDich.cpp

--- BTTH5/BT2/GiaoDich.cpp
+++ BTTH5/BT2/GiaoDich.cpp
@@ -1,13 +1,23 @@
 #include "GiaoDich.h"
 
+// Constructor mặc định
+// Input: không có
+// Output: khởi tạo thành tiền bằng 0
 GiaoDich::GiaoDich()
 {
     thanhTien = 0;
 }
 
+// Destructor
+// Input: không có
+// Output: hủy đối tượng giao dịch
 GiaoDich::~GiaoDich()
 {}
 
+// Nhập thông tin giao dịch
+// Input: mã giao dịch, ngày giao dịch,
+// đơn giá và diện tích từ bàn phím
+// Output: cập nhật thông tin giao dịch
 void GiaoDich::Nhap()
 {
     cin.ignore();
@@ -21,6 +31,9 @@ void GiaoDich::Nhap()
     cin >> dienTich;
 }
 
+// Xuất thông tin giao dịch
+// Input: thông tin giao dịch hiện tại
+// Output: hiển thị thông tin giao dịch
 void GiaoDich::Xuat()
 {
     cout << "Ma GD: " << maGD << endl;
@@ -33,11 +46,17 @@ void GiaoDich::Xuat()
     cout << "Thanh tien: " << thanhTien << endl;
 }
 
+// Lấy thành tiền giao dịch
+// Input: không có
+// Output: giá trị thành tiền
 double GiaoDich::GetThanhTien()
 {
     return thanhTien;
 }
 
+// Lấy ngày giao dịch
+// Input: không có
+// Output: ngày giao dịch
 Date GiaoDich::GetNgayGD()
 {
     return ngayGD;
