cbe1948ca64d0e3314c53adbdea96b45b9ee043d
thienpq2007-afk <thienpq2007@gmail.com>
2026-05-30 20:09:44 +0700

    Implement KiemChungVien class methods

--- BTTH5/BT4/KiemChungVien.cpp
+++ BTTH5/BT4/KiemChungVien.cpp
@@ -1,5 +1,8 @@
 #include "KiemChungVien.h"
 
+// Nhập thông tin kiểm chứng viên
+// Input: thông tin nhân viên cơ bản và số lỗi phát hiện
+// Output: cập nhật thông tin kiểm chứng viên
 void KiemChungVien::Nhap()
 {
     NhanVien::Nhap();
@@ -7,6 +10,9 @@ void KiemChungVien::Nhap()
     cin >> soLoi;
 }
 
+// Xuất thông tin kiểm chứng viên
+// Input: thông tin kiểm chứng viên
+// Output: hiển thị thông tin ra màn hình
 void KiemChungVien::Xuat()
 {
     cout << "\n===== KIEM CHUNG VIEN =====" << endl;
@@ -14,6 +20,11 @@ void KiemChungVien::Xuat()
     cout << "So loi phat hien: " << soLoi << endl;
 }
 
+// Tính lương kiểm chứng viên
+// Input: lương cơ bản, số lỗi phát hiện
+// Output: tiền lương của kiểm chứng viên
+// Thuật giải:
+// Lương = lương cơ bản + số lỗi phát hiện × 50000
 double KiemChungVien::TinhLuong()
 {
     return luongCoBan + soLoi * 50000;
