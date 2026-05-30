deaae85c6ce8d24498b04ae1ca4be33f8f3840ae
thienpq2007-afk <thienpq2007@gmail.com>
2026-05-30 20:17:16 +0700

    Update KhanhHang.cpp

--- BTTH5/BT6/KhanhHang.cpp
+++ BTTH5/BT6/KhanhHang.cpp
@@ -1,6 +1,10 @@
 #include"KhachHang.h"
 #include<fstream>
 
+// Nhập thông tin khách hàng từ file
+// Input: luồng file input chứa:
+// tên khách hàng, số lượng hàng, đơn giá hàng
+// Output: cập nhật thông tin khách hàng
 void KhachHang::Nhap(ifstream& in)
 {
     in.ignore();
@@ -9,6 +13,13 @@ void KhachHang::Nhap(ifstream& in)
     in >> donGiaHang;
 }
 
+// Xuất thông tin khách hàng ra file
+// Input: thông tin khách hàng hiện tại
+// Output: ghi tên khách hàng và số tiền phải trả vào file
+// Thuật giải:
+// Ghi tên khách hàng ra file,
+// sau đó gọi TinhTien() để tính và ghi tổng tiền
+// (sử dụng đa hình nếu TinhTien() là hàm ảo)
 void KhachHang::Xuat(ofstream& out)
 {
     out << tenKhachHang << endl;
